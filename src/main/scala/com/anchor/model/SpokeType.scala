235dd1fe8dbcedf68bbdec7dd63055a571b44aa3
Mesfin Mebrate <mesi91@hotmail.com>
2016-08-12 21:47:04 +0100

    Added SpokeType.

---
+++ src/main/scala/com/anchor/model/SpokeType.scala
@@ -0,0 +1,10 @@
+package com.anchor.model
+
+/**
+ * Created by mesfinmebrate on 12/08/2016.
+ */
+object SpokeType extends Enumeration {
+  val BacklogItem, BufferBlock, ConcreteBlock, DonutCylinder, FinancialTracking, Goal, Hobby,
+  LaserDonut, Portion, Receipt, Saturday, Sunday, Theme, Thread, TimeTable, Weave, Week,
+  WeekDay, Year = Value
+}
